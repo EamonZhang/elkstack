0e59924b6010bf7d2d34c3be3b40448790a027c6
eamon <zhang.eamon@hotmail.com>
2015-11-25 09:59:41 +0800

    modify indices delete and optimize strategy .

--- elasticsearch-curator/command.sh
+++ elasticsearch-curator/command.sh
@@ -1,8 +1,10 @@
 
 
-curator --timeout 36000 --host 10.1.*.* --port 9200 delete indices --older-than 3 --time-unit days --timestring '%Y.%m.%d' --prefix logstash-
-curator --timeout 36000 --host 10.1.*.* --port 9200 optimize --max_num_segments 1 indices  --newer-than 1 --time-unit days --timestring '%Y.%m.%d' --prefix logstash-
+curator --timeout 3600 --host 10.1.*.* --port 9200 delete indices --older-than 6 --time-unit days --timestring '%Y.%m.%d' --prefix logstash-
 
+curator --timeout 36000 --host 10.1.*.* --port 9200 optimize --max_num_segments 1 indices  --newer-than 3 --older-than 1  --time-unit days --timestring '%Y.%m.%d' --prefix logstash-
+
+#save indices within 7 days , optimize indices yestoday
 
 # referto https://github.com/elastic/curator
 
